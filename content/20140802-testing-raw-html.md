8e0af6d3f9bec2e5dde76434136647f407714183
Daniel Chudnov <dchud@gwu.edu>
2014-08-08 12:10:48 -0400

    first commit, clean setup

---
+++ content/20140802-testing-raw-html.md
@@ -0,0 +1,27 @@
+Title:      testing raw html markup
+
+This is a test of raw html.
+
+# This is a markdown h1.
+
+<h1>This is a raw html h1.</h1>
+
+# A test of D3 loading / execution
+
+Below this is raw html to load d3 and draw something.
+
+<div id='viz'></div>
+<script>
+    var width = 400;
+    var height = 200;
+    var svg = d3.select("#viz").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+    var circle = svg.append("circle")
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .attr("r", 100)
+      .attr("fill", "#5DAAD4");
+</script>
+
+Did it work?
---
+++ pelicanconf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'dchud'
+SITENAME = u'data.onebiglibrary.net'
+SITEURL = 'http://localhost:8000'
+#SITEURL = 'http://data.onebiglibrary.net'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_RSS = None
+CATEGORY_FEED_ATOM = None
+TAG_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 0
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'theme' # 'simple' # 'blueidea' # 'notmyidea'
+
+PLUGIN_PATHS = [
+    'plugins', 
+    '/Users/dchud/projects/pelican-plugins',
+    ]
+PLUGINS = [
+    'liquid_tags.liquid_tags',
+    'liquid_tags.notebook',
+    ]
+
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+
+DEFAULT_CATEGORY = ''
+
+#TWITTER_USERNAME = 'dchud'
+
+FILENAME_METADATA = '(?P<date>\d{4}\d{2}\d{2})-(?P<slug>.*)'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
+EXTRA_HEADER = open('_nb_header.html').read().decode('utf-8')
+NOTEBOOK_DIR = '/Users/dchud/projects/data.onebiglibrary.net/content/notebooks'
+
+GOOGLE_ANALYTICS = ''
+
+try:
+    from pelicanconflocal import *
+except:
+    print 'No local settings found.'
